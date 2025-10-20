7951b4ddb26a20ba9086fa1750529ad83e5097a2
mehedimithu <mhasan172036@mscse.uiu.ac.bd>
2025-10-20 22:04:34 +0600

    some validation added

--- tests/bagel/bagel_shop.spec.ts
+++ tests/bagel/bagel_shop.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-
 test("Bagel shop home title is visible", async ({ page }) => {
-
     await page.goto('http://localhost:5173');
 
     await expect(page.getByText("The best bagels in town since 1995!")).toBeVisible();
+
+    await test.step("Validate popup promocode text", async () => {
+        await page.goto('http://localhost:5173', { waitUntil: "load" });
+        
+        const promocodeBtn = page.getByRole("button", { name: "Get Promo Code" });
+
+        // ✅ Wait for popup while clicking the button
+        const [popup] = await Promise.all([
+            page.waitForEvent("popup"),
+            promocodeBtn.click(),
+        ]);
+
+        await expect(popup.getByText("The promo code is:")).toBeVisible();
+    });
 
 });
